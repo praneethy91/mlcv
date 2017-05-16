0d085883fe2e1dc83ebd72a59d79f6dbf01912bb
pranaypareek <pranay.metal@gmail.com>
2017-05-15 23:56:43 -0400

    adds webapp

---
+++ webapp/app.py
@@ -0,0 +1,62 @@
+#!/bin/python
+
+import os
+from flask import Flask, Response, request, abort, render_template, render_template_string, send_from_directory
+from PIL import Image
+import StringIO
+
+app = Flask(__name__)
+
+WIDTH = 1000
+HEIGHT = 800
+
+TEMPLATE = 'index.html'
+
+@app.route('/<path:filename>')
+def image(filename):
+    try:
+        w = int(request.args['w'])
+        h = int(request.args['h'])
+    except (KeyError, ValueError):
+        return send_from_directory('.', filename)
+
+    try:
+        im = Image.open(filename)
+        im.thumbnail((w, h), Image.ANTIALIAS)
+        io = StringIO.StringIO()
+        im.save(io, format='PNG')
+        return Response(io.getvalue(), mimetype='image/png')
+
+    except IOError:
+        abort(404)
+
+    return send_from_directory('.', filename)
+
+@app.route('/')
+def index():
+    images = []
+    for root, dirs, files in os.walk('.'):
+        for filename in [os.path.join(root, name) for name in files]:
+            if not filename.endswith('.png'):
+                continue
+            im = Image.open(filename)
+            w, h = im.size
+            aspect = 1.0*w/h
+            if aspect > 1.0*WIDTH/HEIGHT:
+                width = min(w, WIDTH)
+                height = width/aspect
+            else:
+                height = min(h, HEIGHT)
+                width = height*aspect
+            images.append({
+                'width': int(width),
+                'height': int(height),
+                'src': filename
+            })
+
+    return render_template(TEMPLATE, **{
+        'images': images
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True, host='::')
